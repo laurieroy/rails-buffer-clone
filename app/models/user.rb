7152eaa00407a78922392ab43b277b9fc6435e85
lauretteroy <laurieroy.dev@gmail.com>
2021-02-07 13:27:17 -0700

    formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 	has_secure_password
-	validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, 
-		message: "must be a valid email address" },length: { in: 3..50 }
-	validates :password, length: { in: 6..20 }
+	validates :email, presence: true, uniqueness: true, length: { in: 3..50 },
+		format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+	validates :password, length: { in: 6..25 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  get "about", to: "about#index"
+  get     "about",                to: "about#index"
 
-  get "password", to: "passwords#edit", as: :edit_password
-  patch "password", to: "passwords#update"
+  get     "password",             to: "passwords#edit", as: :edit_password
+  patch   "password",             to: "passwords#update"
 
-  get "sign_up", to: "registrations#new"
-  post "sign_up", to: "registrations#create"
+  get     "sign_up",              to: "registrations#new"
+  post    "sign_up",              to: "registrations#create"
 
-  get "sign_in", to: "sessions#new"
-  post "sign_in", to: "sessions#create"
+  get     "sign_in",              to: "sessions#new"
+  post    "sign_in",              to: "sessions#create"
 
-  delete "logout", to: "sessions#destroy"
+  delete  "logout",               to: "sessions#destroy"
 
-  get "password/reset", to: "password_reset#new"
-  post "password/reset", to: "password_reset#create"
-  get "password/reset/edit", to: "password_reset#edit"
-  patch "password/reset/edit", to: "password_reset#update"
+  get     "password/reset",       to: "password_reset#new"
+  post    "password/reset",       to: "password_reset#create"
+  get     "password/reset/edit",  to: "password_reset#edit"
+  patch   "password/reset/edit",  to: "password_reset#update"
   
   root to: "main#index"
 end
