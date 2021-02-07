6c4cf63e60a47bda2870c7b5bf79256f3db4a513
lauretteroy <laurieroy.dev@gmail.com>
2021-02-06 20:05:39 -0700

    Add password reset mailer

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
 
   get "password/reset", to: "password_reset#new"
   post "password/reset", to: "password_reset#create"
+  get "password/reset/edit", to: "password_reset#edit"
+  patch "password/reset/edit", to: "password_reset#update"
   
   root to: "main#index"
 end
